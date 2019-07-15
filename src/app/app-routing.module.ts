32f2ba8de65e8ad144c66ea0016d7fae7fa694a4
Oliver Enciso <oliverenciso07@gmail.com>
2019-07-15 09:53:46 +0800

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './landingpage/landingpage.module#LandingpageModule' }
+  { path: '', loadChildren: './landingpage/landingpage.module#LandingpageModule' },
+  { path: '*', loadChildren: './landingpage/landingpage.module#LandingpageModule' }
 ];
 
 @NgModule({
